a9ee21713d81fd51b49b25a27437b6454aeba31d
George <sonicgd@gmail.com>
2019-05-15 09:09:26 +0500

    Refactor common lib, move views from site apps to reduce duplications

--- src/BioEngine.BRC.Games/Program.cs
+++ src/BioEngine.BRC.Games/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using BioEngine.BRC.Domain;
-using BioEngine.Core.Logging.Loki;
-using BioEngine.Core.Seo;
-using BioEngine.Core.Site;
-using BioEngine.Extra.Ads;
-using BioEngine.Extra.IPB;
+﻿using BioEngine.BRC.Site;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -20,34 +14,7 @@ namespace BioEngine.BRC.Games
 
         [PublicAPI]
         public static IHostBuilder CreateWebHostBuilder(string[] args) =>
-            new Core.BioEngine(args)
-                .AddPostgresDb()
-                .AddBrcCommon()
-                .AddModule<LokiLoggingModule, LokiLoggingConfig>((configuration, environment) =>
-                    new LokiLoggingConfig(configuration["BRC_LOKI_URL"]))
-                .AddElasticSearch()
-                .AddS3Storage()
-                .AddModule<SeoModule>()
-                .AddModule<IPBSiteModule, IPBModuleConfig>((configuration, env) =>
-                {
-                    if (!Uri.TryCreate(configuration["BE_IPB_URL"], UriKind.Absolute, out var ipbUrl))
-                    {
-                        throw new ArgumentException($"Can't parse IPB url; {configuration["BE_IPB_URL"]}");
-                    }
-
-                    return new IPBModuleConfig(ipbUrl)
-                    {
-                        ApiClientId = configuration["BE_IPB_OAUTH_CLIENT_ID"],
-                        ApiClientSecret = configuration["BE_IPB_OAUTH_CLIENT_SECRET"],
-                        CallbackPath = "/login/ipb",
-                        AuthorizationEndpoint = configuration["BE_IPB_AUTHORIZATION_ENDPOINT"],
-                        TokenEndpoint = configuration["BE_IPB_TOKEN_ENDPOINT"],
-                        ApiReadonlyKey = configuration["BE_IPB_API_READONLY_KEY"]
-                    };
-                })
-                .AddModule<SiteModule, SiteModuleConfig>((configuration, env) =>
-                    new SiteModuleConfig(Guid.Parse(configuration["BE_SITE_ID"])))
-                .AddModule<AdsModule>()
+            new Core.BioEngine(args).AddBrcSite()
                 .GetHostBuilder()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
--- src/BioEngine.BRC.Games/Startup.cs
+++ src/BioEngine.BRC.Games/Startup.cs
@@ -1,45 +1,13 @@
-﻿using BioEngine.BRC.Domain;
-using BioEngine.Core.Logging.Controllers;
-using BioEngine.Core.Site;
-using BioEngine.Extra.Ads.Site;
-using BioEngine.Extra.IPB.Controllers;
-using Microsoft.AspNetCore.Builder;
+﻿using BioEngine.BRC.Site;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace BioEngine.BRC.Games
 {
-    public class Startup : BioEngineStartup
+    public class Startup : BrcSiteStartup
     {
-        private readonly IHostEnvironment _environment;
-
-        public Startup(IConfiguration configuration, IHostEnvironment environment) : base(configuration)
+        public Startup(IConfiguration configuration, IHostEnvironment environment) : base(configuration, environment)
         {
-            _environment = environment;
-        }
-
-        public override void ConfigureServices(IServiceCollection services)
-        {
-            base.ConfigureServices(services);
-            services
-                .AddControllersWithViews()
-                .AddApplicationPart(typeof(LogsController).Assembly)
-                .AddApplicationPart(typeof(UserController).Assembly)
-                .AddApplicationPart(typeof(BrcDomainModule).Assembly)
-                .AddApplicationPart(typeof(AdsSiteController).Assembly);
-            if (_environment.IsDevelopment())
-            {
-                services.AddControllersWithViews().AddRazorRuntimeCompilation();
-            }
-        }
-
-        protected override void ConfigureApp(IApplicationBuilder app, IHostEnvironment env)
-        {
-            base.ConfigureApp(app, env);
-
-            app.UseAuthentication();
-            app.UseAuthorization();
         }
     }
 }
