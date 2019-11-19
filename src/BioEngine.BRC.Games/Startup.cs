4f638e80e687ea54b8dc8c6d04e24a38a6dff5c1
George <sonicgd@gmail.com>
2019-11-19 13:00:05 +0500

    Fix ctros

--- src/BioEngine.BRC.Games/Startup.cs
+++ src/BioEngine.BRC.Games/Startup.cs
@@ -1,12 +1,11 @@
 ﻿using BioEngine.BRC.Site;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 
 namespace BioEngine.BRC.Games
 {
     public class Startup : BrcSiteStartup
     {
-        public Startup(IConfiguration configuration, IHostEnvironment environment) : base(configuration, environment)
+        public Startup(IConfiguration configuration) : base(configuration)
         {
         }
     }
