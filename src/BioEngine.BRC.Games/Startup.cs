436d4bb7a6bfc8c56a1184e997a15e326b493937
George <sonicgd@gmail.com>
2019-11-20 10:59:44 +0500

    Upd startups

--- src/BioEngine.BRC.Games/Startup.cs
+++ src/BioEngine.BRC.Games/Startup.cs
@@ -1,11 +1,12 @@
 ﻿using BioEngine.BRC.Site;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace BioEngine.BRC.Games
 {
     public class Startup : BrcSiteStartup
     {
-        public Startup(IConfiguration configuration) : base(configuration)
+        public Startup(IConfiguration configuration, IHostEnvironment environment) : base(configuration, environment)
         {
         }
     }
